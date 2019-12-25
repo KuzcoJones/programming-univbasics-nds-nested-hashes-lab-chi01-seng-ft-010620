a47288066fe14671250ad1962d9712d7333d0964
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 22:15:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,10 +17,10 @@ def literal_aoh
   # held in the constants
   new_array = [
     {:name => "Don Gately", :occupation => "Live-in Staff"}, 
-    {:name => "Don Gately", :occupation => "Live-in Staff"}, 
-    {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => "Don Gately", :occupation => "Live-in Staff"}]
+{:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate", :occupation => "None"},
+    {:name => "Bruce Green", :occupation => "Live-in Staff"}]
 end
 
 def aoh_lookup(aoh, row, key)
