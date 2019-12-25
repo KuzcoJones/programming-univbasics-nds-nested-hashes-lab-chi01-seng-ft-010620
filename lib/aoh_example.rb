5a5ec9b82e3a7542ed40af4cc792b3329cfdb018
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 22:13:20 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,12 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  new_array = [{:name => "Don Gately", :occupation => "Live-in Staff"}]
+  new_array = [
+    {:name => "Don Gately", :occupation => "Live-in Staff"}, 
+    {:name => "Don Gately", :occupation => "Live-in Staff"}, 
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Don Gately", :occupation => "Live-in Staff"}]
 end
 
 def aoh_lookup(aoh, row, key)
